52d73c41f6328a00f76f7c3b95b734715f20e370
Prince Fefar <64737026+pfefar@users.noreply.github.com>
2022-12-12 16:58:54 -0800

    workflow setup retries and tests (#3)
    * typo
    
    * new key cases
    
    * key list cases
    
    * change to pytest
    
    * add worklfow
    
    * image run
    
    * fix
    
    * try this
    
    * image changes
    
    * typo
    
    * another one
    
    * registry login changes
    
    * changes
    
    * spacing
    
    * registry issue
    
    * err
    
    * change
    
    * credential edit
    
    * err
    
    * try new
    
    * new docker action
    
    * new change
    
    * check
    
    * maybe
    
    * errr
    
    * try
    
    * hate typos fr
    
    * maybe?
    
    * try installing docker itself
    
    * now add back tests
    
    * few cahnges
    
    * docker liveness changes
    
    * try without login
    
    * modifying workflow paths
    
    * typo

---
+++ tests/test_sdk.py
@@ -0,0 +1,65 @@
+import pytest
+from fortress_sdk import Buyer
+
+
+def new_valid_buyer():
+    api_key = "buyer_key"
+    ip_addr = "127.0.0.1"
+    buyer = Buyer(api_key, ip_addr)
+    return buyer
+
+
+def new_invalid_buyer():
+    api_key = "invalid_buyer_key"
+    ip_addr = "127.0.0.1"
+    buyer = Buyer(api_key, ip_addr)
+    return buyer
+
+
+def test_get_key_list_success():
+    buyer = new_valid_buyer()
+    key_list = buyer.get_key_list()
+
+    assert len(key_list) == 1
+
+
+def test_key_list_added_key():
+    buyer = new_valid_buyer()
+
+    key_list = buyer.get_key_list()
+    assert len(key_list) == 1
+
+    _ = buyer.get_key()
+
+    key_list = buyer.get_key_list()
+    assert len(key_list) == 2
+
+
+def test_invalid_buyer_for_key_list():
+    buyer = new_invalid_buyer()
+    key_list = buyer.get_key_list()
+
+    assert key_list == None
+
+
+def test_get_key_success():
+    buyer = new_valid_buyer()
+    new_key = buyer.get_key()
+
+    assert new_key != None
+
+
+def test_invalid_buyer_for_new_key():
+    buyer = new_invalid_buyer()
+    new_key = buyer.get_key()
+
+    assert new_key == None
+
+
+def test_query_success():
+    buyer = new_valid_buyer()
+    sql_query = "select count(*) as numpeople from public.condition_era_death"
+    result, accuracy = buyer.query(query=sql_query)
+
+    assert result != None
+    assert accuracy != ""
