61cc57f5949e014f97cbf761581101739e531398
Prince Fefar <pfefar1@gmail.com>
2023-01-06 13:29:14 -0800

    modify to fixture for tests

--- tests/test_sdk.py
+++ tests/test_sdk.py
@@ -2,79 +2,73 @@ import pytest
 from fortress_sdk import Buyer
 
 
-def new_valid_buyer():
+@pytest.fixture
+def valid_buyer():
     api_key = "buyer_key"
     ip_addr = "127.0.0.1"
     buyer = Buyer(api_key, ip_addr)
     return buyer
 
 
-def new_invalid_buyer():
+@pytest.fixture
+def invalid_buyer():
     api_key = "invalid_buyer_key"
     ip_addr = "127.0.0.1"
     buyer = Buyer(api_key, ip_addr)
     return buyer
 
 
-def test_get_key_list_success():
-    buyer = new_valid_buyer()
-    key_list = buyer.get_key_list()
+def test_get_key_list_success(valid_buyer):
+    key_list = valid_buyer.get_key_list()
 
     assert len(key_list) == 1
 
 
-def test_key_list_added_key():
-    buyer = new_valid_buyer()
-
-    key_list = buyer.get_key_list()
+def test_key_list_added_key(valid_buyer):
+    key_list = valid_buyer.get_key_list()
     assert len(key_list) == 1
 
-    _ = buyer.get_key()
+    _ = valid_buyer.get_key()
 
-    key_list = buyer.get_key_list()
+    key_list = valid_buyer.get_key_list()
     assert len(key_list) == 2
 
 
-def test_invalid_buyer_for_key_list():
-    buyer = new_invalid_buyer()
-    key_list = buyer.get_key_list()
+def test_invalid_buyer_for_key_list(invalid_buyer):
+    key_list = invalid_buyer.get_key_list()
 
     assert key_list == None
 
 
-def test_get_key_success():
-    buyer = new_valid_buyer()
-    new_key = buyer.get_key()
+def test_get_key_success(valid_buyer):
+    new_key = valid_buyer.get_key()
 
     assert new_key != None
 
 
-def test_invalid_buyer_for_new_key():
-    buyer = new_invalid_buyer()
-    new_key = buyer.get_key()
+def test_invalid_buyer_for_new_key(invalid_buyer):
+    new_key = invalid_buyer.get_key()
 
     assert new_key == None
 
 
-def test_query_success():
-    buyer = new_valid_buyer()
+def test_query_success(valid_buyer):
     sql_query = "select count(*) as numpeople from public.condition_era_death"
-    result, accuracy = buyer.query(query=sql_query)
+    result, accuracy = valid_buyer.query(query=sql_query)
 
     assert result != None
     assert accuracy != ""
 
 
-def test_query_history():
-    buyer = new_valid_buyer()
+def test_query_history(valid_buyer):
     sql_query = "select count(*) as numpeople from public.condition_era_death"
-    _ = buyer.query(query=sql_query)
+    _ = valid_buyer.query(query=sql_query)
     sql_query = "select count(*) as numpeople from public.condition_era_death"
-    _ = buyer.query(query=sql_query)
+    _ = valid_buyer.query(query=sql_query)
 
-    assert len(buyer.all_queries) == 2
+    assert len(valid_buyer.all_queries) == 2
 
     sql_query = "select count(*) as numpeople from public.condition_era_death"
-    _ = buyer.query(query=sql_query)
+    _ = valid_buyer.query(query=sql_query)
 
-    assert len(buyer.all_queries) == 3
+    assert len(valid_buyer.all_queries) == 3
